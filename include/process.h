6e5077a5d94aa7f05cf1572620d197acd263fc58
omh18 <46792269+omh18@users.noreply.github.com>
2020-09-02 23:32:11 +0100

    Update process.h

--- include/process.h
+++ include/process.h
@@ -2,12 +2,18 @@
 #define PROCESS_H
 
 #include <string>
+#include "linux_parser.h"
 /*
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
 class Process {
  public:
+  //constructor
+  Process(int Pid_);
+  // update existing pid member variables
+  // Use previous values of Jiffies to produce deltas and get a more current value for CPU usage of process
+  void Update();
   int Pid();                               // TODO: See src/process.cpp
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
@@ -17,7 +23,11 @@ class Process {
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
 
   // TODO: Declare any necessary private members
- private:
+  private:
+  std::string User_, Command_, Ram_;
+  float CpuUtilization_;
+  int Pid_;
+  int long UpTime_, prevAJiffies_;
 };
 
 #endif
