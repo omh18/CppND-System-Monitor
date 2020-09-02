07a6c3ec07dc4621dd6a86cd098b70c6014c4838
omh18 <46792269+omh18@users.noreply.github.com>
2020-09-02 23:31:43 +0100

    Update processor.h

--- include/processor.h
+++ include/processor.h
@@ -3,10 +3,13 @@
 
 class Processor {
  public:
+  // Use previous values of Jiffies to produce deltas and get a more current value for CPU usage
   float Utilization();  // TODO: See src/processor.cpp
 
   // TODO: Declare any necessary private members
  private:
+ long prevAJiffies = 0;
+ long prevJiffies = 0;
 };
 
 #endif
