42c33166b6d14d28b62d7939b5788fafe6c7a89f
omh18 <46792269+omh18@users.noreply.github.com>
2020-09-02 23:22:44 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,12 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  //Use previous values of Jiffies to produce deltas and get a more current value for CPU usage
+  long AJiffies = LinuxParser::ActiveJiffies();
+  long Jiffies = LinuxParser::Jiffies();
+  return ((float) (AJiffies - prevAJiffies) / (float) (Jiffies - prevJiffies));
+  prevAJiffies = LinuxParser::ActiveJiffies();
+  prevJiffies = LinuxParser::Jiffies();
+}
