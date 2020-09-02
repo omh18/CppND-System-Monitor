3c5b3f03f2a137cdbbddfd2749ababb5c02bc0a5
omh18 <46792269+omh18@users.noreply.github.com>
2020-09-02 23:25:30 +0100

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,12 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+	char buffer [9];
+  	long minutes = seconds / 60;
+    int hours = minutes / 60;
+	seconds -= minutes * 60;
+  	minutes -= hours * 60;
+  	snprintf (buffer, 9, "%.2d:%.2d:%.2d", hours, (int) minutes, (int) seconds);
+  	return buffer; 
+}
