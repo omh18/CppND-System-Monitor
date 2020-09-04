93e3e77f10a9c6e529049f2476bd078ddb713e2c
omh18 <46792269+omh18@users.noreply.github.com>
2020-09-04 22:13:01 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -6,7 +6,7 @@ float Processor::Utilization() {
   //Use previous values of Jiffies to produce deltas and get a more current value for CPU usage
   long AJiffies = LinuxParser::ActiveJiffies();
   long Jiffies = LinuxParser::Jiffies();
-  return ((float) (AJiffies - prevAJiffies) / (float) (Jiffies - prevJiffies));
+  return ((float) (AJiffies - prevAJiffies) / (Jiffies - prevJiffies));
   prevAJiffies = LinuxParser::ActiveJiffies();
   prevJiffies = LinuxParser::Jiffies();
 }
