01c7f4dba6ef7eef5115c2809361f4e1221f22ae
omh18 <46792269+omh18@users.noreply.github.com>
2020-09-04 22:08:35 +0100

    Deprecated Update()
    Deprecated Update() and related variables (prevAJiffies)

--- src/process.cpp
+++ src/process.cpp
@@ -16,21 +16,27 @@ Process::Process(int pid) : Pid_(pid){
   Command_ = LinuxParser::Command(pid);
   Ram_ = LinuxParser::Ram(pid);
   UpTime_ = LinuxParser::UpTime(pid);
-  prevAJiffies_ = LinuxParser::ActiveJiffies(pid);
+  //prevAJiffies_ = LinuxParser::ActiveJiffies(pid); <-- Uncomment if Update() works
   
-  CpuUtilization_ = ( (float) prevAJiffies_ / sysconf(_SC_CLK_TCK) ) / (float) UpTime_;
+  CpuUtilization_ = (float) ( LinuxParser::UpTime(pid) / sysconf(_SC_CLK_TCK) ) / (float) UpTime_;
+  // If Update() works
+  // Replace above (process.cpp:23) by "CpuUtilization_ = (float) ( prevAJiffies / sysconf(_SC_CLK_TCK) ) / (float) UpTime_;"
 };
 
+/* //Deprecated, reason: returns very high values (inf/NaN) for CpuUtilization, likely due to division of small time difference
+   //Perhaps add counter variable and take average over last ~3 calls
+   //Maybe instead change UpTime from seconds to Jiffies, and return seconds in system interface functions only
 void Process::Update(){
   // update existing pid member variables
   // Use previous values of Jiffies to produce deltas and get a more current value for CPU usage of process
   int long AJiffies = LinuxParser::ActiveJiffies(Pid_), UpTime = LinuxParser::UpTime(Pid_);
-  CpuUtilization_ = ( (float) (AJiffies - prevAJiffies_) / sysconf(_SC_CLK_TCK) ) / (float) (UpTime - UpTime_);
+  CpuUtilization_ = (float) ( (AJiffies - prevAJiffies_) / sysconf(_SC_CLK_TCK) ) / (UpTime - UpTime_);
   prevAJiffies_ = AJiffies;
   UpTime_ = UpTime;
   Ram_ = LinuxParser::Ram(Pid_);
   // No need to update User and Command
 }
+*/
 
 // TODO: Return this process's ID
 int Process::Pid() { 
